9196e947183108c2b32ba989bd1b707b44341c0c
XavierLopez25 <lop22716@uvg.edu.gt>
2025-01-10 16:39:12 -0600

    fix: project cleanup

---
+++ 04-calculator-app/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+const CalculatorApp = () => {
+  return (
+    <View>
+      <Text>CalculatorApp</Text>
+    </View>
+  );
+};
+export default CalculatorApp;
